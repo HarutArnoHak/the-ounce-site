a2f7cd33710a7272ec6ed8bcbd698a33d0d327cf
haruthak <hak0887@gmail.com>
2023-11-04 23:08:21 -0700

    fix menu and nav overlap

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 import ProductCardContainer from "./ProductCardContainer";
 import ProductCard from "./ProductCard";
 import useProduct from "../hooks/useProduct";
@@ -7,17 +7,20 @@ const ProductGrid = () => {
   const { products } = useProduct();
 
   return (
-    <SimpleGrid
-      columns={{ sm: 1, md: 2, lg: 2, xl: 3 }}
-      spacing={3}
-      padding={10}
-    >
-      {products.map((product) => (
-        <ProductCardContainer key={product.id}>
-          <ProductCard product={product}></ProductCard>
-        </ProductCardContainer>
-      ))}
-    </SimpleGrid>
+    <>
+      <Box height={90}></Box>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 2, xl: 3 }}
+        spacing={3}
+        padding={10}
+      >
+        {products.map((product) => (
+          <ProductCardContainer key={product.id}>
+            <ProductCard product={product}></ProductCard>
+          </ProductCardContainer>
+        ))}
+      </SimpleGrid>
+    </>
   );
 };
 
