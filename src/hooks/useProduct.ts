45c01d1734069aec7edd6291cca462025d9520d4
harutArnoHak <hak0887@gmail.com>
2023-11-05 13:27:25 -0800

    Commit with partial error

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,4 +1,3 @@
-
 // import useData from "./useData";
 
 import product from "../data/product";
@@ -8,41 +7,35 @@ import product from "../data/product";
 // }
 
 export interface Brand {
-
-    id?: number;
-    name?: string
-
+  id?: number;
+  name?: string;
 }
 export interface Photo {
-    id: number,
-    productId: number,
-    fullPath: string
-
+  id: number;
+  productId: number;
+  fullPath: string;
+}
+export interface Inventory {
+  amount?: string | null;
 }
 
 export interface Option {
-    name: string;
-
-
-
+  price: number;
+  inventory: Inventory;
 }
 
 export interface Product {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 
-    // options: Option;
-    photos: Photo[];
-    brand: Brand;
-
-
-
+  options: Option[];
+  photos: Photo[];
+  brand: Brand;
+  isActive: boolean;
 }
 
-
-const useProduct = () => ({ products: product })
+const useProduct = () => ({ products: product });
 export default useProduct;
-
 
 // const useGenres = () => useData<Genre>("/genres")
 // export default useGenres;
